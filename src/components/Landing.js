c3d25e715516415b20b0ba898964c0e0322a0bae
Ryan Yost <ryost217@gmail.com>
2017-01-22 18:53:49 -0600

    messing with routing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,7 +9,7 @@ const Landing = () =>  {
           <Jumbotron id="landing-jumbotron">
             <h1>Welcome to the Rainy Day Fund App</h1>
             <h6>This is a simple practice app meant to help users understand the purpose of a rainy day fund, a.k.a. an <a href="http://www.investopedia.com/terms/e/emergency_fund.asp" target="_blank">emergency fund</a> and illustrate how to establish one. It's NOT personal financial advice and should NOT be considered as such. This is absolutely NOT a substitute for the advice of a licensed professional! <strong>Illustrative purposes only!</strong></h6>
-            <Button href="/#/rainy-day-fund/what-is-a-rainy-day-fund" className="nav-btn">I understand - take me to the app</Button>
+            <Button href="/#/what-is-a-rainy-day-fund" className="nav-btn">I understand - take me to the app</Button>
           </Jumbotron>
         </Grid>
     );
