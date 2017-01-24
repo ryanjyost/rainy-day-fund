eda9ddc324a51fa5aaff36724e11271c4bbb5ec1
Ryan Yost <ryost217@gmail.com>
2017-01-23 22:07:03 -0600

    experiment with Link component on gh pages

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,6 +1,7 @@
 //src/components/MenuBar.js
 
 import React from 'react';
+import {Link} from 'react-router'
 import { ButtonGroup, Button } from 'react-bootstrap';
 
 const Menubar = () => {
@@ -11,7 +12,7 @@ const Menubar = () => {
             <Button className="menu-btn" href="/rainy-day-fund/#/what-is-a-rainy-day-fund">What's a Rainy Day Fund?</Button>
             <Button className="menu-btn" href="/rainy-day-fund/#/assumptions">Assumptions</Button>
             <Button className="menu-btn" href="/rainy-day-fund/#/expenses">Expenses</Button>
-            <Button className="menu-btn" href="/rainy-day-fund/#/savings-plan">Savings Plan</Button>
+            <Button className="menu-btn" href="#"><Link to="savings-plan">Savings Plan</Link></Button>
           </ButtonGroup>
   )
 }
